386c942b3f1966c1fc94449ffc78235fc7eaf5db
AristotleTheThinker <aristotle.piano@gmail.com>
2025-05-17 14:18:56 -0500

    Completed neural network class

--- src/main.py
+++ src/main.py
@@ -2,147 +2,192 @@ import numpy as np
 from scipy.io import loadmat
 import os
 import matplotlib.pyplot as plt
-import time
 
-def input_sigmoid(x):
-    if x > 128:
-        return 1
-    else:
-        return 0
+class Neural_network:
 
-current_dir = os.path.dirname(__file__)  # Src/
-mat_path = os.path.join(current_dir, '../Data/matlab/emnist-letters.mat')
+    def __init__(self):
+        self.input_layer_size = 784 #input of a 28x28 grid
 
-digits_mat = loadmat(mat_path)
+        self.layer2_size = 64 #second layer has 64 neurons, can change
 
-data = digits_mat['dataset']
+        self.layer3_size = 64 #third layer has 64 neurons, can change
 
-# Training data
-train_images = data['train'][0, 0]['images'][0, 0]
-train_labels = data['train'][0, 0]['labels'][0, 0]
+        self.output_layer_size = 36 #outputs a number or character
 
-# Testing data
-test_images = data['test'][0, 0]['images'][0, 0]
-test_labels = data['test'][0, 0]['labels'][0, 0]
+        self.learning_rate = 0.01
 
-# Reshape images to 28x28
-train_images = train_images.reshape((-1, 28, 28)).astype(np.uint8)
-test_images = test_images.reshape((-1, 28, 28)).astype(np.uint8)
+        self.input_neurons = []
 
-#Rotate the images
-train_images = np.transpose(train_images, (0, 2, 1))
-test_images = np.transpose(test_images, (0, 2, 1))
+        self.layer2_neurons = []
 
-input_layer_size = 784 #input of a 28x28 grid
+        self.input_to2connections = []
 
-layer2_size = 64 #second layer has 64 neurons, can change
+        self.layer3_neurons = []
 
-layer3_size = 64 #third layer has 64 neurons, can change
+        self.layer2to3connections = []
 
-output_layer_size = 36 #outputs a number or character
+        self.output_layer_neurons = []
 
-input_neurons = []
+        self.layer3to_output_connections = []
 
-layer2_neurons = []
+        self.target_arr = []
 
-input_to2connections = []
+        class Neuron:
+            def __init__(self, val, bias, index):
+                self.val = val
+                self.bias = bias
+                self.position = index
 
-layer3_neurons = []
+            def set_val(self, new_val):
+                self.val = new_val
 
-layer2to3connections = []
+            def add_to_val(self, add_val):
+                self.val += add_val
 
-output_layer_neurons = []
+        class Connection: #connection has a weight, bias, and is connected to 2 neurons
+            def __init__(self, weight, neuron_in, neuron_out):
+                self.weight = weight
+                self.neuron_in = neuron_in
+                self.neuron_out = neuron_out
 
-layer3to_output_connections = []
+            def set_weight(self, new_weight):
+                self.weight = new_weight
 
-train_image_1 = train_images[0]
+        #Creates input neuron objects
+        for i in range(self.input_layer_size):
+            instance = Neuron(0, 0, i)
+            self.input_neurons.append(instance)
 
-class Neuron:
-    def __init__(self, val, bias):
-        self.val = val
-        self.bias = bias
+        #Creates layer2 neuron objects
+        for i in range(self.layer2_size):
+            instance = Neuron(0, np.random.uniform(-0.01, 0.01), i)
+            self.layer2_neurons.append(instance)
 
-    def set_val(self, new_val):
-        self.val = new_val
+        #Creates input_to2 connections
+        for neuron_in in self.input_neurons:
+            for neuron_out in self.layer2_neurons:
+                instance = Connection(np.random.uniform(-0.01, 0.01), neuron_in, neuron_out)
+                self.input_to2connections.append(instance)
 
-    def add_to_val(self, add_val):
-        self.val += add_val
+        #Creates layer3 neuron objects
+        for i in range(self.layer3_size):
+            instance = Neuron(0, np.random.uniform(-0.01, 0.01), i)
+            self.layer3_neurons.append(instance)
 
-class Connection: #connection has a weight, bias, and is connected to 2 neurons
-    def __init__(self, weight, neuron_in, neuron_out):
-        self.weight = weight
-        self.neuron_in = neuron_in
-        self.neuron_out = neuron_out
+        #Creates layer2to3connections
+        for neuron_in in self.layer2_neurons:
+            for neuron_out in self.layer3_neurons:
+                instance = Connection(np.random.uniform(-0.01, 0.01), neuron_in, neuron_out)
+                self.layer2to3connections.append(instance)
 
-    def set_weight(self, new_weight):
-        self.weight = new_weight
+        #Creates output layer neurons
+        for i in range(self.output_layer_size):
+            instance = Neuron(0, np.random.uniform(-0.01, 0.01), i)
+            self.output_layer_neurons.append(instance)
 
-    def set_bias(self, new_bias):
-        self.bias = new_bias
+        #Creates layer3to_output_connections
+        for neuron_in in self.layer3_neurons:
+            for neuron_out in self.output_layer_neurons:
+                instance = Connection(np.random.uniform(-0.01, 0.01), neuron_in, neuron_out)
+                self.layer3to_output_connections.append(instance)
 
-#Creates input neuron objects
-for i in range(input_layer_size):
-    instance = Neuron(0, 0)
-    input_neurons.append(instance)
+        neurons = [self.input_neurons, self.layer2_neurons, self.layer3_neurons, self.output_layer_neurons]
 
-#Creates layer2 neuron objects
-for i in range(layer2_size):
-    instance = Neuron(0, np.random.uniform(-0.01, 0.01))
-    layer2_neurons.append(instance)
+        connections = [self.input_to2connections, self.layer2to3connections, self.layer3to_output_connections]
 
-#Creates input_to2 connections
-for neuron_in in input_neurons:
-    for neuron_out in layer2_neurons:
-        instance = Connection(np.random.uniform(-0.01, 0.01), neuron_in, neuron_out)
-        input_to2connections.append(instance)
+    @staticmethod
+    def input_sigmoid(x):
+        if x > 128:
+            return 1
+        else:
+            return 0
+       
+    @staticmethod    
+    def relu(x):
+        return np.maximum(0, x)
 
-#Creates layer3 neuron objects
-for i in range(layer3_size):
-    instance = Neuron(0, np.random.uniform(-0.01, 0.01))
-    layer3_neurons.append(instance)
+    @staticmethod
+    def der_relu(x):
+        if x == 0:
+            return 0
+        else:
+            return 1
 
-#Creates layer2to3connections
-for neuron_in in layer2_neurons:
-    for neuron_out in layer3_neurons:
-        instance = Connection(np.random.uniform(-0.01, 0.01), neuron_in, neuron_out)
-        layer2to3connections.append(instance)
+    def fill_inputs_create_target_array(self, image, label_num):
+        #Fills input neurons with data
+        for i in range(len(image)):
+            for j in range(len(image[i])):
+                self.input_neurons[28*i+j].set_val(self.input_sigmoid(image[i][j]))
 
-#Creates output layer neurons
-for i in range(output_layer_size):
-    instance = Neuron(0, np.random.uniform(-0.01, 0.01))
-    output_layer_neurons.append(instance)
+        for i in range(36):
+            self.target_arr.append(0)
+        self.target_arr[label_num-1] = 1
 
-#Creates layer3to_output_connections
-for neuron_in in layer3_neurons:
-    for neuron_out in output_layer_neurons:
-        instance = Connection(np.random.uniform(-0.01, 0.01), neuron_in, neuron_out)
-        layer3to_output_connections.append(instance)
+    def calculate_neuron_vals(self, arr, arr2):
+        for connection in arr:
+            input = connection.neuron_in.val
+            weight = connection.weight
+            output = input * weight
+            connection.neuron_out.add_val(output)
 
-neurons = [input_neurons, layer2_neurons, layer3_neurons, output_layer_neurons]
+        for neuron in arr2:
+            neuron.add_to_val(neuron.bias)
+            neuron.set_val(self.relu(neuron.val))
 
-connections = [input_to2connections, layer2to3connections, layer3to_output_connections]
+    def propagate_forward(self):
+        #calculates the output of the first layer to each neuron in the second layer
+        self.calculate_neuron_vals(self.input_to2connections, self.layer2_neurons)
+        
+        self.calculate_neuron_vals(self.layer2to3connections, self.layer3_neurons)
 
-def fill_inputs():
-    #Fills input neurons with data
-    for i in range(len(train_image_1)):
-        for j in range(len(train_image_1[i])):
-            input_neurons[28*i+j].set_val(input_sigmoid(train_image_1[i][j]))
+        self.calculate_neuron_vals(self.layer3to_output_connections, self.output_layer_neurons)
 
-def calculate_neuron_vals(arr, arr2):
-    for connection in arr:
-        input = connection.neuron_in.val
-        weight = connection.weight
-        output = input * weight
-        connection.neuron_out.add_val(output)
+    @staticmethod
+    def calculate_output_error(target, actual):
+        return target - actual
 
-    for neuron in arr2:
-        neuron.add_to_val(neuron.bias)
 
-def propagate_forward():
-    #calculates the output of the first layer to each neuron in the second layer
-    calculate_neuron_vals(input_to2connections, layer2_neurons)
-    
-    calculate_neuron_vals(layer2to3connections, layer3_neurons)
+    #Backpropagation
+    def backpropagation(self):
+        
+        output_error = []
 
-    calculate_neuron_vals(layer3to_output_connections, output_layer_neurons)
+        layer3_error = []
 
+        for i in range(len(self.layer3_neurons)):
+            layer3_error.append(0)
+
+        layer2_error = []
+
+        for i in range(len(self.layer2_neurons)):
+            layer2_error.append(0)
+
+        #Output Errors
+        for i in range(len(self.output_layer_neurons)):
+            error = self.calculate_output_error(self.target_arr[i], self.output_layer_neurons[i].val)
+            output_error.append(error)
+
+        #Reset Layer3toOutput Weights + Get Layer 3 errors
+        for connection in self.layer3to_output_connections:
+            error_index = connection.neuron_out.position
+            error = output_error[error_index]
+            layer3_error[connection.neuron_in.position] += error * connection.weight
+            layer3_error[connection.neuron_in.position] *= self.der_relu(self.layer3_neurons[connection.neuron_in.position].val)
+            connection.set_weight(connection.weight + self.learning_rate * error * connection.neuron_in.val)
+
+        #Reset Layer2to3 Weights + Get Layer 2 errors
+        for connection in self.layer2to3connections:
+            error_index = connection.neuron_out.position
+            error = layer3_error[error_index]
+            layer2_error[connection.neuron_in.position] += error * connection.weight
+            layer2_error[connection.neuron_in.position] *= self.der_relu(self.layer2_neurons[connection.neuron_in.position].val)
+            connection.set_weight(connection.weight + self.learning_rate * error * connection.neuron_in.val)
+
+        #Reset InputTo2 Weights
+        for connection in self.input_to2connections:
+            error_index = connection.neuron_out.position
+            error = layer2_error[error_index]
+            connection.set_weight(connection.weight + self.learning_rate * error * connection.neuron_in.val)
+
+
+        
---
+++ src/training.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.io import loadmat
+import os
+import main
+
+current_dir = os.path.dirname(__file__)  # Src/
+mat_path = os.path.join(current_dir, '../Data/matlab/emnist-letters.mat')
+
+digits_mat = loadmat(mat_path)
+
+data = digits_mat['dataset']
+
+# Training data
+train_images = data['train'][0, 0]['images'][0, 0]
+train_labels = data['train'][0, 0]['labels'][0, 0]
+
+# Testing data
+test_images = data['test'][0, 0]['images'][0, 0]
+test_labels = data['test'][0, 0]['labels'][0, 0]
+
+# Reshape images to 28x28
+train_images = train_images.reshape((-1, 28, 28)).astype(np.uint8)
+test_images = test_images.reshape((-1, 28, 28)).astype(np.uint8)
+
+#Rotate the images
+train_images = np.transpose(train_images, (0, 2, 1))
+test_images = np.transpose(test_images, (0, 2, 1))
+
+neural_network = main.Neural_network()
+
+neural_network.fill_inputs_create_target_array(train_images[0],train_labels[0])
+neural_network.propagate_forward()
+for i in range(20):
+    print(neural_network.layer3to_output_connections[i].weight)
+neural_network.backpropagation()
+for i in range(20):
+    print(neural_network.layer3to_output_connections[i].weight)
+
+
+#for i in range(len(train_images)):
+#    main.fill_inputs_create_target_array(train_images[i],train_labels[i])
+#    main.propagate_forward()
+#    main.backpropagation()
